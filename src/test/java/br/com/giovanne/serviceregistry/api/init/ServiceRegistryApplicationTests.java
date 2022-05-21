df4c8cac6f7ee36576d0f8a74a940c96415eef23
Giovanne Lisboa Rodrigues <giovannelrodrigues@gmail.com>
2022-05-20 22:22:36 -0300

    service registry

---
+++ src/test/java/br/com/giovanne/serviceregistry/api/init/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giovanne.serviceregistry.api.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
